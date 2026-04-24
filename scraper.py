8762da62640380817bc3d89f3f38985bd701d9f4
Birrus09 <simone.verganj@gmail.com>
2026-04-24 13:26:06 +0200

    Implement scrape function in scraper.py
    Added a scrape function to handle web scraping with parameters.

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
 import requests
+
+def scrape(map, dumpname, robots= True, modes = [0, 1, 0, 0], sizelimit = 20000000, wordslog = "AND", words = ["lorem", "ipsum"]):
+    mapindex = 0
+    with open(map, "r") as map:
+        while True:
+            url = map.readline().split()[0]
+            res = requests.get(url)
+
+
+            #do stuff
+
+            mapindex += 1
+            
