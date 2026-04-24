26631a3bb4c0f137b219c7352244ec713e6c6316
Birrus09 <simone.verganj@gmail.com>
2026-04-24 13:27:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
 
         elif command[0] == "help":
             print("dirbuster, create a map of directories starting from a given root: dirbust <url> <wordlist> -r (get report) -o (add rescode options separated by space)")
-            print("spider/scraper, scrape data from a site using a given directory map: scrape <main url> <directory map> -f filename -i (ignore robots.txt --NOT RECCOMENDED--) -o open filter options")
+            print("spider/scraper, scrape data from a site using a given directory map: scrape <directory map> -f filename -i (ignore robots.txt --NOT RECCOMENDED--) -o open filter options")
+            print("options are: raw (download all data), find all links, search for words, download objects")
 
         elif command[0] == "dirbust":
             options = [200, 301, 302]
@@ -34,7 +35,32 @@ def main():
             dirbuster.dirbust(command[1], f"wordlists/{command[2]}", options = options, report = report)
 
         elif command[0] == "scrape":
-            pass
+            
+            map = command[1]
+            if command[1][0] != "/":
+                command[1] = "maps/" + command[1]
+
+            if '-f' in command:
+                f_index = command.index('-f')
+                dumpname = command[f_index + 1]
+            else:
+                dumpname = "dump.txt"
+            if '-i' in command:
+                robots = False
+            else:
+                robots = True
+
+            if '-o' in command:
+                modes = [0, 1, 0, 0]
+                words = ["lorem", "ipsum"]
+                wordslog = "AND"
+                sizelimit = 200000000
+                print("raw    href    words    objects    words logic    sizelimit    words")
+                print(f"{modes[0]}    {modes[1]}    {modes[2]}    {modes[3]}    {wordslog}    {sizelimit}    {words}")
+                opt = input("input index to modify")
+                #code a menu or something to change filter options
+
+            scraper.scrape(map, dumpname, robots, modes, sizelimit, wordslog, words)
 
         else:
             print(f"Unknown command: {command[0]}")
